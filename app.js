823e488379a2e1c5d6213088d3cb3632e377c879
Kashyap 2210 <kash.cdac@gmail.com>
2024-03-09 01:12:15 +0530

    Comments added in JS file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.querySelectorAll(".section .sub-section").forEach((section) => {
-  section.addEventListener("click", function () {
-    const targetId = this.textContent.toLowerCase();
-    const target = document.getElementById(targetId);
-    if (target) {
+  section.addEventListener("click", function () { //Event listner for section.
+    const targetId = this.textContent.toLowerCase(); // We store which sub-section is clicked.
+    const target = document.getElementById(targetId); //We select the sub-section clicked.
+    if (target) { //If target exists then we do scroll event.
       window.scrollTo({
         top: target.offsetTop - 100,
         behavior: "smooth",
