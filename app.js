47cd2ff13fbc7f59c63c9c982c74c6cc2e1fc89d
Kashyap 2210 <kash.cdac@gmail.com>
2024-03-09 00:02:26 +0530

    Nav-Bar Functionality Added

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// script.js
-
 document.querySelectorAll(".section .sub-section").forEach((section) => {
   section.addEventListener("click", function () {
-    const targetId = this.textContent.toLowerCase(); // Assuming section names match IDs of sections on the page
+    const targetId = this.textContent.toLowerCase();
     const target = document.getElementById(targetId);
     if (target) {
       window.scrollTo({
-        top: target.offsetTop,
+        top: target.offsetTop - 100,
         behavior: "smooth",
       });
     }
